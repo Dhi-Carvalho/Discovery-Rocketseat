c8c880a35cb5560b99d4ef4fa53ee891ebe6368d
Diogo Santana <dhi.carvalho85@gmail.com>
2021-01-14 16:52:24 -0300

    JS Star Guide Exercise

---
+++ Projeto02/school_grade_system/scripts.js
@@ -0,0 +1,34 @@
+function getScore(score) {
+    let scoreA = score >= 90 && score <= 100
+    let scoreB = score  >= 80 && score <= 89
+    let scoreC = score  >= 70 && score <= 79
+    let scoreD = score  >= 60 && score <= 69
+    let scoreF = score  < 60 && score >= 0
+
+    let scoreFinal;
+
+    if (scoreA) {
+        scoreFinal = "A"
+    } else if (scoreB) {
+        scoreFinal = "B"
+    } else if (scoreC) {
+        scoreFinal = "C"
+    } else if (scoreD) {
+        scoreFinal = "D"
+    } else if (scoreF) {
+        scoreFinal = "F"
+    } else {
+        scoreFinal = "Nota inválida"
+    }
+
+    return scoreFinal;
+}
+
+console.log(getScore(102))
+console.log(getScore(12))
+console.log(getScore(-5))
+console.log(getScore(93))
+console.log(getScore(82))
+console.log(getScore(77))
+console.log(getScore(63))
+console.log(getScore(52))
